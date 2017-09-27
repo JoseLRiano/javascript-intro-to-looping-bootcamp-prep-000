481235e1622506f0c03a4c0a6a9de585352d6722
Jose Riano <joselriano10@gmail.com>
2017-09-27 03:57:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,13 @@
 function forLoop(array){
-
   for(let n=0;n<25;n++){
     array.push('I am ${i} strange loop ${i===0 ? \'\' : \'s\' }.')
   }
   return array;
+}
+function whileLoop(n){
+  while(n>0){
+    console.log(n);
+    n--;
+  }
+  console.log("done");
 }
