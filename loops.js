0db290ba819942821b3b5d88a484aab4b9499c1b
Jose Riano <joselriano10@gmail.com>
2017-09-27 03:12:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forloop(arr){
-var arr[25];
-for(var i=1;1<=arr.lenght;i++){
-  if(i===1){
+
+for(let i=1;1<=25;i++){
+  if(i==="1"){
     arr[i]="I am 1 strange loop.";
   }else {
     arr[i]="I am ${i} strange loops.";
