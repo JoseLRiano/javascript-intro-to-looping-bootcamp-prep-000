e2c2d505f4486f00f1b23748276d4ff6d8a510c8
Jose Riano <joselriano10@gmail.com>
2017-09-27 04:08:58 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array){
+  for(let n=0;n<25;n++){
+    array.push('I am ${i} strange loop ${i===0 ? \'\' : \'s\' }.');
+  }
+  return array;
+}
+function whileLoop(n){
+  while(n>0){
+    console.log(n);
+    n--;
+  }
+  return "done";
+}
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+function doWhileLoop(array){
+  do{
+    array.pop();
+    return array;
+  }while(arra.length > 0 && maybeTrue());
+}
