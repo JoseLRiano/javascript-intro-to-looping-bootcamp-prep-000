9d8541ec08b15e2f8caeb2239ef5dbeb79858228
Jose Riano <joselriano10@gmail.com>
2017-09-27 03:27:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forloop(array){
 for(let n=0;n<25;n++){
   if(n==0){
-    array[n]="I am ${n} strange loop.";
+    array[n]="I am "+n+" strange loop.";
   }else {
-    array[n]="I am ${n} strange loops.";
+    array[n]="I am "+n+" strange loops.";
   }
 }
 return array;
