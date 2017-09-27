8efbd51d79818d6cfd8e97ba0944a5147f5bc0c8
Jose Riano <joselriano10@gmail.com>
2017-09-27 03:35:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ for(let n=0;n<25;n++){
   if(n==0){
     array.push("I am 1 strange loop.");
     else{
-        array.push("I am ${n} strage loops.");
+        array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
     }
   }
 }
